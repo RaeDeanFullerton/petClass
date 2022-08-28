07b7ea76ec7b2e9553b01d4c4f5617bd53c0a98c
RaeDeanFullerton <101029021+RaeDeanFullerton@users.noreply.github.com>
2022-08-28 15:21:26 -0700

    first/final commit

---
+++ Pet/src/pet.java
@@ -0,0 +1,17 @@
+
+public class pet {
+	String name;
+	int age;
+	String location;
+	String type;
+	
+	public pet() {
+		
+	}
+	publc pet(String name, int age, String location, String type) {
+		this.name = name;
+		this.age = age;
+		this.location = location;
+		this.type = type;
+	}
+}
